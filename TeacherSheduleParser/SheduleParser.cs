15321927ad896df5af53402af875219d29d18ea5
mrtnvm <mrtnv.m@gmail.com>
2015-06-12 01:48:54 +0300

    Скачивание расписания и вывод всех данных из него на консоль

---
+++ TeacherSheduleParser/SheduleParser.cs
@@ -0,0 +1,47 @@
+﻿using Excel;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TeacherSheduleParser
+{
+    public class SheduleParser
+    {
+        private IExcelDataReader _Reader;
+        private DataSet _Data;
+
+        private bool _IsNotEnd = true;
+
+        public SheduleParser(FileStream stream)
+        {
+            string fileType = stream.Name.Split('.').Last();
+            if (fileType != "xls" && fileType != "xlsx")
+                throw new Exception("File have not correct format");
+
+            if (fileType == "xls")
+                _Reader = ExcelReaderFactory.CreateBinaryReader(stream);
+            else
+                _Reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            _Data = _Reader.AsDataSet();
+        }
+
+        public string ReadRow()
+        {
+            if (!_IsNotEnd)
+                return null;
+
+            var row = new StringBuilder();
+            for (int i = 0; i < _Reader.FieldCount; i++)
+            {
+                row.Append(_Reader.GetString(i));
+            }
+
+            _IsNotEnd = _Reader.Read();
+
+            return row.ToString();
+        }
+    }
+}
---
+++ TryTeacherSheduleParser/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.IO;
+using System.Net;
+using TeacherSheduleParser;
+
+namespace TryTeacherSheduleParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string remoteUri = "http://www.ulstu.ru/schedule/teachers/%a8%ad.%ef%a7..xls";
+            string fileName = "foreign_languages.xls";
+            var webClient = new WebClient();
+
+            webClient.DownloadFile(remoteUri, fileName);
+
+            var file = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            var parser = new SheduleParser(file);
+
+            WriteLine("================== ВЫВОД ДАННЫХ ======================");
+            string row;
+            while ((row = parser.ReadRow()) != null)
+                WriteLine(row);
+            WriteLine("================== КОНЕЦ ВЫВОДA ======================");
+
+            ReadLine();
+        }
+    }
+}
